270047e826e149ea0350b5dd49bd4e9c8dc66444
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-12 15:08:07 -0500

    CORRECTED INFO

--- src/components/ui/Cliente/ProyectosCliente.jsx
+++ src/components/ui/Cliente/ProyectosCliente.jsx
@@ -125,7 +125,7 @@ const ProyectoClientes = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                   </svg>
                   <span>
-                    Encargado: {proyecto.encargado ? `${proyecto.encargado.nombre} ${proyecto.encargado.apellido}` : 'No asignado'}
+                    Encargado: {proyecto.encargado ? `${proyecto.encargado.datos.nombre} ${proyecto.encargado.datos.apellido}` : 'No asignado'}
                   </span>
                 </div>
                 
--- src/components/ui/Encargado/ProyectosEncargado.jsx
+++ src/components/ui/Encargado/ProyectosEncargado.jsx
@@ -132,7 +132,7 @@ const ProyectoEncargado = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                   </svg>
                   <span>
-                    Cliente: {proyecto.cliente ? `${proyecto.cliente.nombre} ${proyecto.cliente.apellido}` : 'No asignado'}
+                    Cliente: {proyecto.cliente ? `${proyecto.cliente.datos.nombre} ${proyecto.cliente.datos.apellido}` : 'No asignado'}
                   </span>
                 </div>
                 
