7623e6611a79c125284c17590594b8e2098e3d4e
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-04-11 20:49:02 -0500

    CORRECTED COLOR HOVER

--- src/components/ui/Encargado/Proyecto/BarraProgresoProyecto.jsx
+++ src/components/ui/Encargado/Proyecto/BarraProgresoProyecto.jsx
@@ -194,7 +194,7 @@ const BarraProgresoProyecto = ({ proyectoId }) => {
       <div className="fixed bottom-4 right-4 md:hidden z-10 flex flex-col space-y-2">
         <Link 
           to={`/encargado/proyecto/chat/${proyectoId}`} 
-          className="flex items-center justify-center bg-green-500 text-white rounded-full w-14 h-14 shadow-lg border-2 border-white transition duration-300 ease-in-out hover:bg-blue-600"
+          className="flex items-center justify-center bg-green-500 text-white rounded-full w-14 h-14 shadow-lg border-2 border-white transition duration-300 ease-in-out hover:bg-green-600"
         >
           <svg className="w-6 h-6" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
@@ -203,7 +203,7 @@ const BarraProgresoProyecto = ({ proyectoId }) => {
         
         <Link 
           to={`/encargado/proyecto/ar/${proyectoId}`} 
-          className="flex items-center justify-center bg-green-600 text-white rounded-full w-14 h-14 shadow-lg border-2 border-white transition duration-300 ease-in-out hover:bg-blue-700"
+          className="flex items-center justify-center bg-green-600 text-white rounded-full w-14 h-14 shadow-lg border-2 border-white transition duration-300 ease-in-out hover:bg-green-700"
         >
           <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 18.5C15.5899 18.5 18.5 15.5899 18.5 12C18.5 8.41015 15.5899 5.5 12 5.5C8.41015 5.5 5.5 8.41015 5.5 12C5.5 15.5899 8.41015 18.5 12 18.5Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
